fa4318c23763a07a9bef2d0b28cc566e24e25611
Ruta-Dsp <ruta@dsps.it>
2022-02-14 12:23:37 +0100

    first commit

---
+++ maps.py
@@ -0,0 +1,19 @@
+import streamlit as st 
+import pandas as pd 
+import numpy as np 
+import pydeck as pdk 
+import altair as alt 
+from datetime import datetime
+
+
+
+DATA_URL = ('https://raw.githubusercontent.com/nikkisharma536/streamlit_app/master/covid.csv')
+@st.cache
+def load_data():
+    data = pd.read_csv(DATA_URL)
+    data['Date'] = pd.to_datetime(data['Date']).dt.strftime('%Y-%m-%d')
+    return data
+df = load_data()
+
+# show data on streamlit
+ st.write(df)
